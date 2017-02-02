3d5cec1d6bc250e4c2774f7b89f4a462739f7273
Anton Mitkovets <raisiel@yandex.ru>
2017-02-02 22:45:12 +0300

    Add daemon2 and modify docker-compose

---
+++ hosting_daemon2/daemon2.py
@@ -0,0 +1,44 @@
+import json
+import urllib.request
+import subprocess
+import shutil
+
+from collections import namedtuple
+from pathlib import Path
+
+API_BASE = 'http://localhost:8000/api'
+API_SHOULD_ENABLE_APPS = API_BASE + '/apps-to-enable'
+
+
+class App:
+    APPS_HOME = Path('.')
+
+    def __init__(self, json_description):
+        self.app_type = json_description['app_type']
+        self.path = App.APPS_HOME / json_description['app_path']
+        self.app_url = json_description['app_url']
+        self.repo_url = json_description['repo_url']
+        
+    def prepare_app_folders(self):
+        try:
+            self.path.mkdir(parents=True)
+        except FileExistsError as e:
+            print(e)
+        shutil.rmtree(str(self.path.home()))
+   
+    def _git_clone_app_code(self):
+        pass
+        
+
+def get_apps_to_enable():
+    response = urllib.request.urlopen(API_SHOULD_ENABLE_APPS).read()
+    response = str(response, 'utf-8')
+    response = json.loads(response)
+    apps = response.get('response', [])
+    return [App(app) for app in apps]
+    
+        
+if __name__ == '__main__':
+    apps = get_apps_to_enable()
+    for app in apps:
+        app.prepare_app_folders()
--- web_interface/hosting_web_interface/settings.py
+++ web_interface/hosting_web_interface/settings.py
@@ -73,13 +73,26 @@ WSGI_APPLICATION = 'hosting_web_interface.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('POSTGREAVAILABLE'):
+    DATABASES = {
+        'default': {
+	        'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'pgsuperuser',
+            'PASSWORD': 'pgsuperuserpassword',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+if DEBUG:
+    print('Database:', DATABASES)
 
 
 # Password validation
